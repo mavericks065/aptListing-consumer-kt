618d8f4cca02d5c50cb8fd8de695c7e3aa1b49fc
Nicolas Guignard <nicolas065@gmail.com>
2023-12-03 19:49:17 +0100

    init project with spring

---
+++ src/main/kotlin/com/nig/aptListing/AptListingApplication.kt
@@ -0,0 +1,11 @@
+package com.nig.aptListing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AptListingApplication
+
+fun main(args: Array<String>) {
+	runApplication<AptListingApplication>(*args)
+}
---
+++ src/test/kotlin/com/nig/aptListing/AptListingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nig.aptListing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AptListingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
